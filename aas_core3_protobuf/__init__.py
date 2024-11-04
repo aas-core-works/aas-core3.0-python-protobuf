ef58fba0bf46998f414124c81ebbcc6fed6a1e86
Marko Ristin <marko.ristin@gmail.com>
2024-11-04 05:47:55 +0100

    Release 0.0.2 (#6)
    We publish this new version to test the publication pipeline.

--- aas_core3_protobuf/__init__.py
+++ aas_core3_protobuf/__init__.py
@@ -1,6 +1,6 @@
 """Convert AAS instances between Python and Python ProtoBuf objects."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Marko Ristin"
 __copyright__ = "2024 Contributors to aas-core3.0-python-protobuf"
 __license__ = "License :: OSI Approved :: MIT License"
--- docs/source/changelog.rst
+++ docs/source/changelog.rst
@@ -2,6 +2,10 @@
 Change Log
 **********
 
+0.0.2 (2024-11-04)
+==================
+We publish this new version to test the publication pipeline.
+
 0.0.1 (2024-11-03)
 ==================
 This is the first initial version ready for first trials.
